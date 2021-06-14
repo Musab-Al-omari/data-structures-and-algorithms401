4291e0feea6aaae146043e33ad17fd294612bd87
Musab-Al-omari <musabsomari@gmail.com>
2021-06-14 16:37:54 +0300

    haha

---
+++ challenges/margeSort/margeSort.js
@@ -0,0 +1,44 @@
+function mergeSort(arr) {
+    let n = arr.length;
+    if (n > 1) {
+      let mid = Math.floor(n / 2);
+      let left = arr.slice(0, mid);
+      let right = arr.slice(mid);
+      console.log(left);
+      console.log(right);
+      mergeSort(left);
+      mergeSort(right);
+      merge(left, right, arr);
+    }
+    return arr;
+ 
+}
+  function merge(left, right, arr) {
+    let i = 0;
+    let j = 0;
+    let k = 0;
+    while (i < left.length && j < right.length) {
+      if (left[i] <= right[j]) {
+        arr[k] = left[i];
+        i++;
+      } else {
+        arr[k] = right[j];
+        j++;
+      }
+      k++;
+    }
+    if (i === left.length) {
+      arr[k] = right[j];
+      j++;
+      k++;
+    } else {
+      arr[k] = left[i];
+      i++;
+      k++;
+    }
+  }
+  
+console.log(mergeSort([3,8,5,2,6]));
+  
+  module.exports = mergeSort;
+
